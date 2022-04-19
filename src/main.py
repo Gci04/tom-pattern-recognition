202d46751ad808787ec5c13cdca899c728c10182
Gci04 <gcinzoe04@gmail.com>
2022-04-19 10:56:18 +0300

    Add pattern search, plot and distict motif search

--- src/main.py
+++ src/main.py
@@ -1,14 +1,81 @@
 from utils import *
 import numpy as np
 
-if __name__ == '__main__':
-    all_data = get_data()
-    Ts = [v for _, v in all_data.items()]
-    m = 14
+all_data = get_data()
+del all_data['xavzelada/repo_test']
 
-    central_radius, central_Ts_idx, central_subseq_idx = consensus_motif(Ts, m)
-    q = Ts[central_Ts_idx][central_subseq_idx:central_subseq_idx+m]
+def approach1(Ts, m, threshold_dist=0.01, save_plots=False):
+    '''Maximize the number of matches of Q. For each repo time series find a
+    pattern and then match with all other repositories time series'''
+    result = {}
+    N = len(Ts)
+    for i in range(N):
+        _ , q = get_topk_motifs(Ts[i], k=5, m=m)
+        count = 0
+        for j in range(N):
+            if i == j: continue
+            distance_profile = search_pattern(Ts[j], q[0], max_distance=threshold_dist)
+            count += len(distance_profile)
 
+        result[i] = count
+    if result[0] > 0: print(result)
+    selected_t = Ts[max(result, key=result.get)]
+    mp = stumpy.stump(selected_t, m=m)
+    top_k_motifs_idx = np.argsort(mp[:, 0])[:10]
+
+    if save_plots :
+        plot_patterns(selected_t, mp, top_k_motifs_idx, m, save=save_plots)
+
+def approach2(Ts, m):
+    '''Join all the repositories and find a pattern'''
+    joinedTs = np.concatenate(Ts)
+    print(joinedTs.shape)
+    _, q = get_topk_motifs(joinedTs, k=1, m=m)
     print(q)
+
+def approach3(Ts, m):
+    '''Find the z-normalized consensus pattern of multiple time series'''
+    central_radius, central_Ts_idx, central_subseq_idx = consensus_motif(Ts, m)
+    consensus_pattern = Ts[central_Ts_idx][central_subseq_idx:central_subseq_idx+m]
+
+    tot_found = 0
+    overall_distance = 0.0
+    min_found = np.inf
+    min_indx = central_Ts_idx
+
+    print(f'Consensus pattern : {*consensus_pattern,}')
+    print(f'Central radius : {central_radius}')
+
     for i in range(len(Ts)):
-        print(search_pattern(Ts[i], q)[:2])
+        if central_Ts_idx == i : continue
+        distance_profile = search_pattern(Ts[i], consensus_pattern)
+        tot_found += len(distance_profile)
+        if len(distance_profile) == 0:
+            min_indx = 0
+            continue
+        overall_distance += distance_profile[:,0].sum()
+        if len(distance_profile) < min_found:
+            min_found = len(distance_profile)
+            min_indx = i
+
+    print(f'Total Distance : {overall_distance}')
+    print(f'Total Found patterns : {tot_found}')
+    print(f'Min Found patterns : {min_found}, indx: {min_indx}')
+
+def print_repos_stats():
+    global all_data
+    for k, v in all_data.items():
+        print(f'{k} : {len(v)} commits')
+
+if __name__ == '__main__':
+    Ts = [v[:500] for _, v in all_data.items()]
+    print_repos_stats()
+    # m = 14
+    # approach1(Ts, m)
+    # approach2(Ts, m)
+    # approach3(Ts, m)
+    
+    for m in [5, 7, 14, 21]:
+        for d in [0.001, 0.01, 0.1, 1.0, 2.0]:
+            print(f'({m}, {d})')
+            approach1(Ts,  m=m, threshold_dist=d)
--- src/utils.py
+++ src/utils.py
@@ -19,14 +19,23 @@ def get_data(data_path='../Data/sample_data.zip', target_metric='total_changed')
         result[name] = group.sort_index()[target_metric].values.astype(np.float64)
     return result
 
-def search_pattern(T, query_pattern):
-    distance_profile = stumpy.match(query_pattern, T)
+def search_pattern(T, query_pattern, max_distance=1.0):
+    distance_profile = stumpy.match(query_pattern, T, max_distance=max_distance)
     return distance_profile
 
 def get_topk_motifs(T, k=5, m=7):
     mp = stumpy.stump(T, m=m)
     top_k_motifs_idx = np.argsort(mp[:, 0])[:k]
     return top_k_motifs_idx, np.array([T[i:i+m] for i in top_k_motifs_idx])
+
+def get_topk_distict_motifs(T, m=7, min_neighbors=5):
+    '''A subsequence, Q, becomes a candidate motif if there are at least min_neighbor number
+    of other subsequence matches in T (outside the exclusion zone) with a distance less
+    or equal to max_distance.'''
+
+    mp = stumpy.stump(T, m=m)
+    motif_distances, motif_indices = stumpy.motifs(T, mp[:, 0], min_neighbors=min_neighbors)
+    return motif_distances, motif_indices
 
 def UnanchoredChain(T, m=7):
     mp = stumpy.stump(T, m=m)
@@ -37,4 +46,18 @@ def get_pattern(repo, m=7):
     return repo, indx, motifs
 
 def consensus_motif(Ts, m):
-    return stumpy.ostinato(Ts,m)
+    return stumpy.ostinato(Ts, m)
+
+def plot_patterns(T, mp, patterns_index, m, save=False):
+    fig, axs = plt.subplots(2,1,sharex=True, figsize=(10,7), gridspec_kw={'hspace': 0})
+    axs[0].set_ylabel('T', fontsize='20')
+    axs[0].plot(T)
+    axs[1].plot(mp[:,0],c='green', linewidth=2)
+    axs[1].set_ylabel('P1', fontsize='20')
+
+    for idx in patterns_index:
+      axs[0].plot(np.arange(idx, idx+m), T[idx:idx+m], c='red', linewidth=2)
+      axs[0].axvline(x=idx, linestyle='dashed', c='black')
+
+    if save :
+        plt.savefig(f'All_data_{m}.eps')
