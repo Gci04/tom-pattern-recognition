92a273c83fc2dc778ae1f86b7c96ad33364e01e2
Gci04 <gcinzoe04@gmail.com>
2022-06-08 16:31:02 +0000

    Tune params

--- src/main.py
+++ src/main.py
@@ -5,36 +5,41 @@ import json, sys
 from argparse import ArgumentParser
 
 def approach1(Ts, p, z, d=0.3333):
-    R = []
+    R = set()
     all_patterns = {}
-    for m in np.arange(3,7):
+    for m in np.arange(5,15):
         k = 0
         print(f'subsequence : {m}')
         central_radius, central_Ts_idx, central_subseq_idx = consensus_motif(Ts, m)
         consensus_pattern = Ts[central_Ts_idx][central_subseq_idx:central_subseq_idx+m]
 
-        match_collection = {}
+        
         print(f'Consensus pattern : {*consensus_pattern,}')
         print(f'Radius {central_radius}')
-        for i in range(len(Ts)):
-            distance_profile = search_pattern(Ts[i], consensus_pattern, max_distance=central_radius*0.5)
-            if distance_profile is None: continue
-            tmatches = len(distance_profile)
-            if tmatches > z and tmatches < 6:
-                match_collection[i] = distance_profile[:,1]
-                k += 1
-        if k > 0 :
-            pr = k/len(Ts)
-            print(pr)
-            if pr > 0.05 and pr < 0.20:
-                all_patterns[m] = {}
-                all_patterns[m]['consensus_pattern'] = consensus_pattern
-                all_patterns[m]['patterns'] = match_collection
-                all_patterns[m]['percentage'] = pr
+        for ii, threshold in enumerate([central_radius/e for e in range(1,10)]):
+            match_collection = {}
+            k = 0
+            for i in range(len(Ts)):
+                distance_profile = search_pattern(Ts[i], consensus_pattern, max_distance=threshold)
+                if distance_profile is None: continue
+                tmatches = len(distance_profile)
+                if tmatches > z and tmatches < 6:
+                    match_collection[i] = distance_profile[:,1]
+                    k += 1
+            if k > 0 :
+                pr = k/len(Ts)
+                #print(threshold,pr)
+                if pr > 0.05 and pr < 0.20:
+                    print(f'threshold:{round(threshold,4)}, percentage: {round(pr,4)}')
+                    all_patterns[m] = {}
+                    all_patterns[m][f'threshold-{round(threshold,4)}'] = {}
+                    all_patterns[m][f'threshold-{round(threshold,4)}']['consensus_pattern'] = consensus_pattern
+                    all_patterns[m][f'threshold-{round(threshold,4)}']['patterns'] = match_collection
+                    all_patterns[m][f'threshold-{round(threshold,4)}']['percentage'] = round(pr,4)
 
-        if k < p and k > len(Ts)*0.05:
-            print(f'adding : {*consensus_pattern,}')
-            R.append(consensus_pattern)
+            if k < p and k > len(Ts)*0.05:
+                print(f'adding : {*consensus_pattern,}')
+                R.add(tuple(consensus_pattern))
 
     return R, all_patterns
 
@@ -44,13 +49,8 @@ def get_patterns(target_metric, all_data):
     i = 0
     for k, v in all_data.items():
         repo_timestamps = v.get('time_stamps')
-        last_commit = max(repo_timestamps)
-        if last_commit.is_leap_year and last_commit.day > 28:
-            last_commit = last_commit.replace(day = last_commit.day - 2)
-
-        mask = np.where(last_commit.replace(year = last_commit.year - 1) > repo_timestamps)[0]
-        res = v.get(target_metric)[mask]
-        if(len(res) > 10):
+        res = v.get(target_metric)
+        if(len(res) > 15):
             Ts.append(res)
             projects_names_map[i] = k
             i+=1
@@ -64,12 +64,17 @@ def get_patterns(target_metric, all_data):
     overall_result = {}
     for m, sub_data in allPatterns.items():
         subsequence_result = {}
-        for i, patterns_found in sub_data['patterns'].items():
-            repo_name = projects_names_map[i]
-            temp = {}
-            for n, p in enumerate(patterns_found,1):
-                temp[f'position {n}'] = [datetime.strftime(l,'%Y-%m-%d %H:%M:%S') for l in all_data[repo_name]['time_stamps'][p:p+5]]
-            subsequence_result[repo_name] = temp
+        for thrshld, thrshld_sub_data  in sub_data.items():
+            thrshld_result = {}
+            for i, patterns_found in thrshld_sub_data['patterns'].items():
+                repo_name = projects_names_map.get(i)
+                temp = {}
+                for n, p in enumerate(patterns_found,1):
+                    temp[f'position {n}'] = [l.strftime('%Y-%m-%d %H:%M:%S') for l in all_data[repo_name]['time_stamps'][p:p+m]]
+                thrshld_result[repo_name] = temp
+            subsequence_result[thrshld] = thrshld_result
+            subsequence_result[thrshld]['percentage'] = thrshld_sub_data['percentage']
+            subsequence_result[thrshld]['consensus_pattern'] =  thrshld_sub_data['consensus_pattern'].tolist()
         overall_result[str(m)] = subsequence_result
 
     print('Saving result ...')
